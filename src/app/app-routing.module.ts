bb6905ba05b1ee88dfdf37e24666ecafe14c969f
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2021-02-21 23:35:56 -0500

    login menu entrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EntradaComponent } from './components/entrada/entrada.component';
 import { LoginComponent } from './components/login/login.component';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path:'Login', component:LoginComponent},
+  { path:'Login', component:LoginComponent},
   { path: 'AcercaDe', component:AcercaDeComponent },
+  { path: 'MainMenu', component:MainMenuComponent, canActivate:[AuthGuard]},
+  { path: 'Entry', component:EntradaComponent, canActivate:[AuthGuard]},
   { path: '', redirectTo: '/Login', pathMatch: 'full' },
   { path: '**', redirectTo: '/Login', pathMatch: 'full' }
 
--- src/app/services/empleados-service.service.ts
+++ src/app/services/empleados-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
@@ -10,17 +11,29 @@ export class EmpleadosServiceService {
   private url:string = environment.apiUrl+"api/v1/empleados"
 
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient:HttpClient,
+              private router:Router) { }
   
   createTokenHeader():HttpHeaders{
     let token=localStorage.getItem('token');
+   
     let headers=new HttpHeaders({'Authorization':token});
     return headers;
   }
 
   findByEmailAndPassword(email:string,password:string):Observable<any>{
+   
     let headers=this.createTokenHeader();
-    return this.httpClient.post(this.url+"/"+email+"/"+password ,{headers:headers})
+    return this.httpClient.get(this.url+"/signIn/"+email+"/"+password ,{headers:headers})
+  }
+
+
+  logOut(){
+    
+    localStorage.removeItem('user');
+    
+    this.router.navigate(['/Login']);
+
   }
 
 }
